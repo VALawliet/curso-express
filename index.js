3ca4b986e6ee08bb2c0998396cfcc1649e62c0e5
Law <victorestrada2006@gmail.com>
2023-08-10 00:18:27 +0200

    Empezando a jugar con endpoints distintos y mandando json al cliente

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res)=>{
   res.send('Hello, world');
 });
 
+app.get('/nueva-ruta', (req, res)=>{
+
+  res.send("I'm a new endpoint");
+});
+
+app.get('/products', (req, res)=>{
+  res.json({
+    name: 'uwu',
+    precio: 10000
+  })
+})
+
 app.listen(port, ()=>{
   console.log(`port ${port}`);
 });
