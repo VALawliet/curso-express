cd044c3059cedbb9a9d1060ee9cf8488ea82f544
Law <victorestrada2006@gmail.com>
2023-08-10 00:13:39 +0200

    Inicio del server con express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('uwu')
+const express = require('express');
+
+const app = express();
+
+const port = 6969;
+
+app.get('/', (req, res)=>{
+
+  res.send('Hello, world');
+});
+
+app.listen(port, ()=>{
+  console.log(`port ${port}`);
+});
+
