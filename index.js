143e9355035269e1b2df101ae5ad7984533f22a6
Law <victorestrada2006@gmail.com>
2023-08-10 02:06:27 +0200

    identificadores

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 6969;
 
 app.get('/', (req, res)=>{
 
+
   res.send('Hello, world');
 });
 
@@ -14,14 +15,49 @@ app.get('/nueva-ruta', (req, res)=>{
   res.send("I'm a new endpoint");
 });
 
+app.get('/home', (req, res)=>{
+  res.send('Acá se debería cargar html, js y css');
+})
+
 app.get('/products', (req, res)=>{
+  res.json([
+    {
+      name: 'uwu',
+      precio: 2000
+    },
+
+    {
+      name: 'owo',
+      precio: 3000
+    }
+  ])
+})
+
+app.get('/products/:id', (req, res)=>{
+  const {id} = req.params;
   res.json({
+    productId: id,
     name: 'uwu',
-    precio: 10000
+    price: 2000
   })
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res)=>{
+
+  const {categoryId, productId} = req.params;
+
+  res.json({
+    name: 'omg',
+    precio: 2999,
+    categoryId,
+    productId
+
+  });
 })
 
 app.listen(port, ()=>{
   console.log(`port ${port}`);
 });
+
+
 
