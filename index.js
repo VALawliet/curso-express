042e74b6e838aedb2e844705551382ac27b9cdb1
Lawliet <victorestrada2006@gmail.com>
2023-08-10 02:26:50 +0200

    Creando productos dinamicos usando los endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 
 const app = express();
 
@@ -20,17 +21,24 @@ app.get('/home', (req, res)=>{
 })
 
 app.get('/products', (req, res)=>{
-  res.json([
-    {
-      name: 'uwu',
-      precio: 2000
-    },
 
-    {
-      name: 'owo',
-      precio: 3000
-    }
-  ])
+  const proudctos = [];
+  const { size } = req.query;
+  const limit = size || 10
+
+  for(let index = 0; index < limit; index++){
+    proudctos.push({
+      name:faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url()
+    })
+  }
+
+  res.json(proudctos)
+})
+
+app.get('/products/filter', (req, res)=>{
+  res.send('yo soy un filter');
 })
 
 app.get('/products/:id', (req, res)=>{
@@ -41,6 +49,23 @@ app.get('/products/:id', (req, res)=>{
     price: 2000
   })
 });
+
+
+
+app.get('/users', (req, res)=>{
+  
+  const {limit, offset} = req.query;
+
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }else{
+    res.send('No hay parametros');
+  }
+
+})
 
 app.get('/categories/:categoryId/products/:productId', (req, res)=>{
 
